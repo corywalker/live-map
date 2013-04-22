ae2a1e0c6fab77f03da0e394ee09fbdd299498ee
cmwslw <cwalker32@gmail.com>
2013-04-22 01:41:28 -0400

    Now has the right height.

--- map.js
+++ map.js
@@ -12,7 +12,8 @@ var HomepageMap = {
 
         this.projection = d3.geo.mercator()
             .scale((width + 1) / 2 / Math.PI)
-            .translate([width / 2, height / 2])
+            .translate([width / 2, height / 2 - 170])
+            .clipExtent([[0,0],[960,660-170]])
             .precision(.1);
 
         var path = d3.geo.path()
@@ -20,7 +21,7 @@ var HomepageMap = {
 
         var svg = d3.select("body").append("svg")
             .attr("width", width)
-            .attr("height", height);
+            .attr("height", 660-170);
 
         d3.json("world-50m.json", function(error, world) {
           svg.insert("path")
